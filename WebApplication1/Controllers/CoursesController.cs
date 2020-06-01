150815c6b12cdcfacca1d220b22c7a2c79f2b746
namphan532 <namphan532@gmail.com>
2020-06-01 11:17:54 +0700

    Add project files.

---
+++ WebApplication1/Controllers/CoursesController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+using WebApplication1.ViewModels;
+
+namespace WebApplication1.Controllers
+{
+    public class CoursesController : Controller
+    {
+  
+        private readonly ApplicationDbContext _dbContext;
+
+        public CoursesController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+
+
+        // GET: Courses
+        [Authorize]
+        public ActionResult Create()
+        {
+            var viewModel = new CourseViewModel
+            {
+                Categories = _dbContext.Categories.ToList()
+            };
+            return View(viewModel);
+        }
+
+
+
+        // GET: Courses
+        [Authorize]
+        [HttpPost]
+        public ActionResult Create(CourseViewModel viewModel)
+        {
+            var course = new Course
+            {
+                LecturedId = User.Identity.GetUserId(),
+                DateTime = viewModel.GetDateTime(),
+                CategoryId = viewModel.Category,
+                Place = viewModel.Place
+
+            };
+            _dbContext.Courses.Add(course);
+            _dbContext.SaveChanges();
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ WebApplication1/Models/ICategory.cs
@@ -0,0 +1,8 @@
+﻿namespace WebApplication1.Models
+{
+    public interface ICategory
+    {
+        byte Id { get; set; }
+        string Name { get; set; }
+    }
+}
